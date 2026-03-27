c54c7d215cb3bbde06653392d5bb77bd3d01d083
Garieldas <dainiusrainys99@gmail.com>
2026-03-27 22:01:46 +0200

    -wip

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,45 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User
+from .models import Client, CompanyProfile, Invoice, InvoiceItem, User, UserProfile
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     ordering = ('email',)
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'email', 'bank_account', 'bank_name')
+
+
+@admin.register(CompanyProfile)
+class CompanyProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company_name', 'company_code', 'vat_code', 'city')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'client_type', 'company_code', 'vat_code', 'email', 'city')
+    list_filter = ('client_type',)
+    search_fields = ('name', 'company_code', 'vat_code', 'email')
+
+
+class InvoiceItemInline(admin.TabularInline):
+    model = InvoiceItem
+    extra = 1
+    fields = ('description', 'unit', 'quantity', 'unit_price')
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_number', 'client', 'status', 'issue_date', 'due_date', 'tax_enabled', 'tax_rate')
+    list_filter = ('status', 'tax_enabled', 'currency')
+    search_fields = ('series', 'number', 'client__name')
+    inlines = [InvoiceItemInline]
+
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
