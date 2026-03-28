93a9425f7ddf239031794362f2c11cac74892c1b
Garieldas <dainiusrainys99@gmail.com>
2026-03-28 21:05:13 +0200

    -static

--- core/settings.py
+++ core/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'app' / 'static_resources',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
